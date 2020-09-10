7d217487a0e22a8d61d00d690335c7a616fd47d3
FioraLove <3434279505@qq.com>
2020-09-10 18:30:40 +0800

    更新README.md开发日志

--- src/views/HiddenSource.vue
+++ src/views/HiddenSource.vue
@@ -58,7 +58,7 @@ export default {
 <style scoped>
     .source{
         background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898;
- background-blend-mode: multiply,multiply;
+        background-blend-mode: multiply,multiply;
         width: 100wvw;
         height:100vh;
     }
