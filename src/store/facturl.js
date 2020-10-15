a045634f7468643d74f945efe5c2bf9d80e903b9
FioraLove <3434279505@qq.com>
2020-10-15 18:34:06 +0800

    新增短视频解析接口，前后分离签名算法以及调整【工具】模块UI

--- src/store/facturl.js
+++ src/store/facturl.js
@@ -1,5 +1,7 @@
-const baseUrl = "http://127.0.0.1:8001";
+const baseUrl = "http://127.0.0.1:8002";
+const signature = "0#badwoman%-_-%#0";
 
 export default{
-    baseUrl
+    baseUrl,
+    signature
 }
