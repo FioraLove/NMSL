d8b919499f8f9999f1b98f9ee4628901eaa0c75e
FioraLove <3434279505@qq.com>
2020-12-26 15:30:26 +0800

    删除token提示文件

---
+++ src/store/facturl.example.js
@@ -0,0 +1,10 @@
+const baseUrl = "http://127.0.0.1:8001";
+const signature = "0#badwoman%-_-%#0";
+// pixiv-token申请地址：https://www.acg-gov.com/ 注册登录获取token
+const pixiv_token = "";
+
+export default{
+    baseUrl,
+    signature,
+    pixiv_token
+}
